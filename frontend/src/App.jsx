5cd332611b85aa16663c7201ccf469cdfc953ec9
Mpdanish <danishmaliyekkal786@gmail.com>
2024-09-17 21:03:23 +0530

    folder structured

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LayoutRoutes from './routes/LayoutRoutes/LayoutRoutes'
+
+function App() {
+
+  return (
+    <>
+    <LayoutRoutes/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/routes/LayoutRoutes/LayoutRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../../pages/Login/Login";
+
+
+function LayoutRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default LayoutRoutes;
