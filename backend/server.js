151c8fb2e8f812ab3bc6d62a587b617b91603ec3
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-09-22 05:27:22 +0530

    admin login and context privateroute authmanagment completed

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,43 @@
 import express from "express";
+import cors from "cors"; // Import CORS package
+import 'dotenv/config';
+import router from "./Router.js";
 
+// Initialize the Express application
+const app = express();
+
+// Define a port to listen on
+const PORT = process.env.PORT || 5000;
+
+// Middleware configuration
+
+// Set up CORS with options (adjust the origin as needed)
+const corsOptions = {
+    origin: [
+      'http://localhost:5173', // Add your frontend URL
+      'http://localhost:4200',
+      'http://localhost:5000',
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allows cookies to be sent from the frontend
+  };
+  
+  app.use(cors(corsOptions));
+  
+
+// Parse incoming JSON requests (replaces body-parser.json())
+app.use(express.json());
+
+// Parse URL-encoded requests (replaces body-parser.urlencoded())
+app.use(express.urlencoded({ extended: true }));
+
+// Use your defined routes
+app.use('/api', router);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
