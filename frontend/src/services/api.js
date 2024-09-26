4c2dc0ea53dc139670d7d12df3bf3d0d24d8e22e
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-09-26 11:04:43 +0530

    api added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "https://sealand.vercel.app/api",
+  baseURL: "https://sealand-api.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
