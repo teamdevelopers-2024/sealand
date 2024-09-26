649ba917c6a75055234c2e2348a572fd7b71d9fc
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-09-26 17:00:04 +0530

    hello

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,17 +1,28 @@
 import mongoose from "mongoose";
 
+let isConnected; // Tracks the connection status
 
-function connectDB(){
-    mongoose.set("strictQuery" , false)
-    mongoose.connect(process.env.MONGO_URL)
-    .then(()=>{
-        console.log("Database connected successfully!!")
-    }).catch((error)=>{
-        console.log(`data base conncetion error ${error}`)
-    })
+async function connectDB() {
+    if (isConnected) {
+        console.log("Using existing database connection");
+        return;
+    }
+
+    if (!process.env.MONGO_URL) {
+        throw new Error('MONGO_URL is not defined in environment variables');
+    }
+
+    try {
+        mongoose.set("strictQuery", false);
+        const db = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = db.connections[0].readyState;
+        console.log("Database connected successfully!");
+    } catch (error) {
+        console.error(`Database connection error: ${error.message}`);
+    }
 }
-
- 
-
 
 export default connectDB;
