11c6539250a284477853f5972d81c1626fdec432
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-09-26 17:22:12 +0530

    mongo and vercel.json changed

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
 
-let isConnected; // Tracks the connection status
+let isConnected = false;
 
 async function connectDB() {
-    if (isConnected) {
-        console.log("Using existing database connection");
-        return;
-    }
+  if (isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
 
-    if (!process.env.MONGO_URL) {
-        throw new Error('MONGO_URL is not defined in environment variables');
-    }
+  if (!process.env.MONGO_URL) {
+    throw new Error("MONGO_URL is not defined in environment variables");
+  }
 
-    try {
-        mongoose.set("strictQuery", false);
-        const db = await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        isConnected = db.connections[0].readyState;
-        console.log("Database connected successfully!");
-    } catch (error) {
-        console.error(`Database connection error: ${error.message}`);
-    }
+  try {
+    mongoose.set("strictQuery", false);
+    const db = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true, // Optional: depending on your version
+      useUnifiedTopology: true // Optional: depending on your version
+    });
+    isConnected = db.connections[0].readyState;
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.error(`Database connection error: ${error.message}`);
+  }
 }
 
 export default connectDB;
